d13b423bef718ffec83dd3825448191571aedd28
UdithaIshan <uditha.ishan34@gmail.com>
2021-03-25 00:31:36 +0530

    deprecated bodyparser removed & form validation +

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
-const bodyParser = require('body-parser')
+const userRepo = require('./repositories/users')
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     res.send(`
@@ -33,8 +33,16 @@ app.get('/', (req, res) => {
 //         next();
 // };
 
-app.post('/', (req, res) => {
-    res.send(req.body);
+app.post('/', async (req, res) => {
+    const {email, pwd, pwdConfirm} = req.body;
+    const existingUser = await userRepo.getOneBy({email: email});
+    if(existingUser) {
+        return res.send("<p>Email is already in use!</p>");
+    }
+    if(pwd !== pwdConfirm) {
+        return res.send("<p>Passwords must match</p>");
+    }
+    res.send("<p>Account created!</p>");
 });
 
 app.listen('3000', () => {
