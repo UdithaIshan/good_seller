4d62e498bec7b7a71b6c1669d24e88f709511a0f
UdithaIshan <uditha.ishan34@gmail.com>
2021-04-08 12:19:36 +0530

    extended classes

--- repositories/users.js
+++ repositories/users.js
@@ -1,29 +1,10 @@
-const fs = require("fs");
 const crypto = require('crypto');
 const util = require('util');
+const Repository = require("./repository");
 
 const scrypt = util.promisify(crypto.scrypt);
 
-class UsersRepository {
-  constructor(filename) {
-    if (!filename) {
-      throw new Error("Filename is required");
-    }
-    this.filename = filename;
-
-    try {
-      fs.accessSync(this.filename);
-    } catch (error) {
-      fs.writeFileSync(this.filename, "[]");
-    }
-  }
-
-  async getAll() {
-    return JSON.parse(
-      await fs.promises.readFile(this.filename, { encoding: "utf8" })
-    );
-  }
-
+class UsersRepository extends Repository{
   async create(userData) {
     userData['id'] = this.randomId();
     const salt = crypto.randomBytes(8).toString('hex');
@@ -35,45 +16,6 @@ class UsersRepository {
     await this.writeAll(records);
 
     return record;
-  }
-
-  async writeAll(records) {
-    await fs.promises.writeFile(this.filename, JSON.stringify(records, null, 2));
-  }
-
-  randomId() {
-    return crypto.randomBytes(4).toString('hex');
-  }
-
-  async getOne(id) {
-    const data = await this.getAll();
-    return data.find((record) => record.id === id);
-  }
-
-  async delete(id) {
-    const data = await this.getAll();
-    const filterdData = data.filter((record) => record.id !== id);
-    await this.writeAll(filterdData);
-  }
-
-  async getOneBy(filters) {
-    const data = await this.getAll();
-    for (const [key, val] of Object.entries(filters)) {
-      const match = data.find((record) => record[key] === val)
-      if(match)
-        return match;
-    }
-  }
-
-  async update(id, params) {
-    const data = await this.getAll();
-    const record = data.find((record) => record.id === id);
-
-    if(!record) {
-      throw new Error(`No record for ${id}`);
-    }
-
-    Object.assign(record, params);
   }
 
   async compare(saved, input) {
