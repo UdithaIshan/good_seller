83242ea66bd4638449dad3c5b88e94231901ca6e
UdithaIshan <uditha.ishan34@gmail.com>
2021-04-08 12:19:09 +0530

    products routes

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const { validationResult } = require('express-validator');
+const newTemplate = require('../../views/admin/product/new');
+const { titleValidator, priceValidator } = require('../validations');
 
 const router = express.Router();
 
@@ -7,7 +10,13 @@ router.get('/admin/products', (req,res) => {
 });
 
 router.get('/admin/products/new', (req, res) => {
-
+    res.send(newTemplate({}));
 });
+
+router.post('/admin/products/new', [titleValidator, priceValidator], (req, res) => {
+    const errors = validationResult(req);
+    console.log(errors);
+    return res.send(newTemplate({}))
+})
 
 module.exports = router;
